6dafecb7950200cf18f2e5f7e8bdeb745f4c1684
Andre <andre69190@googlemail.com>
2026-07-24 15:27:54 +0200

    Bioglow PWA

---
+++ sw.js
@@ -0,0 +1,29 @@
+/**
+ * BIOGLOW Service Worker – macht die App installierbar & offline-relaunchfähig.
+ * Same-origin: network-first (online frisch, offline aus Cache).
+ * Fremde APIs (iNaturalist/GBIF/Wikipedia): nur Netz, nichts cachen.
+ */
+const CACHE = 'bioglow-2';
+const SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).catch(() => {}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  const { request } = e;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(request).then((hit) => hit || caches.match('./index.html')))
+  );
+});
